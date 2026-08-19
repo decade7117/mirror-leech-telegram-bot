2274cfb87c91c0207b3ff42df18bd79213284d7f
decade7117 <78193337+decade7117@users.noreply.github.com>
2026-08-19 13:52:29 +0700

    Update __init__.py

--- bot/modules/__init__.py
+++ bot/modules/__init__.py
@@ -1,7 +1,6 @@
 from .bot_settings import send_bot_settings, edit_bot_settings
 from .cancel_task import cancel, cancel_multi, cancel_all_buttons, cancel_all_update
 from .chat_permission import authorize, unauthorize, add_sudo, remove_sudo
-from .clone import clone_node
 from .exec import aioexecute, execute, clear
 from .file_selector import select, confirm_selection
 from .force_start import remove_from_queue
@@ -9,31 +8,15 @@ from .gd_count import count_node
 from .gd_delete import delete_file
 from .gd_search import gdrive_search, select_type
 from .help import arg_usage, bot_help
-from .mirror_leech import (
-    mirror,
-    leech,
-    qb_leech,
-    qb_mirror,
-    jd_leech,
-    jd_mirror,
-    nzb_leech,
-    nzb_mirror,
-)
 from .restart import (
     restart_bot,
     restart_notification,
     confirm_restart,
 )
-from .rss import get_rss_menu, rss_listener
-from .search import torrent_search, torrent_search_update, initiate_search_tools
-from .nzb_search import hydra_search
 from .services import start, ping, log
-from .shell import run_shell
 from .stats import bot_stats, get_packages_version
 from .status import task_status, status_pages
 from .users_settings import get_users_settings, edit_user_settings, send_user_settings
-from .ytdlp import ytdl, ytdl_leech
-from . import multi_uploader  # noqa: F401
 from . import bilibili_login  # noqa: F401
 
 __all__ = [
@@ -47,10 +30,8 @@ __all__ = [
     "unauthorize",
     "add_sudo",
     "remove_sudo",
-    "clone_node",
     "aioexecute",
     "execute",
-    "hydra_search",
     "clear",
     "select",
     "confirm_selection",
@@ -60,27 +41,13 @@ __all__ = [
     "gdrive_search",
     "select_type",
     "arg_usage",
-    "mirror",
-    "leech",
-    "qb_leech",
-    "qb_mirror",
-    "jd_leech",
-    "jd_mirror",
-    "nzb_leech",
-    "nzb_mirror",
     "restart_bot",
     "restart_notification",
     "confirm_restart",
-    "get_rss_menu",
-    "rss_listener",
-    "torrent_search",
-    "torrent_search_update",
-    "initiate_search_tools",
     "start",
     "bot_help",
     "ping",
     "log",
-    "run_shell",
     "bot_stats",
     "get_packages_version",
     "task_status",
@@ -88,8 +55,5 @@ __all__ = [
     "get_users_settings",
     "edit_user_settings",
     "send_user_settings",
-    "ytdl",
-    "ytdl_leech",
-    "multi_uploader",
     "bilibili_login",
 ]
